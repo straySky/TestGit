23de92d2c51646784f89deea330ee15eea955f17
wx.s <1059455914@qq.com>
2017-05-05 15:54:44 +0800

    second

--- app/src/main/java/com/example/administrator/testgit/MainActivity.java
+++ app/src/main/java/com/example/administrator/testgit/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         int aa = 0;
+        int bb = 1;
     }
 }
